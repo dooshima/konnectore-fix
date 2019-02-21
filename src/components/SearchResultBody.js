0caeb1bf727bf37f94501884e5a35fbad1e24bd4
Saidi Adegoke <saidiadegokea@gmail.com>
2019-02-21 17:47:12 +0100

    Reposition ContestListItem

--- src/components/SearchResultBody.js
+++ src/components/SearchResultBody.js
@@ -37,7 +37,7 @@ import FeedCard from './FeedCard';
 import ImageCard from './UI/Posts/ImageCard/ImageCard';
 import MasonryGrid from './UI/MasonryGrid/MasonryGrid';
 import TextCard from './UI/Posts/TextCard/TextCard';
-import ContestListItem from './UI/Contests/ContestListItem/ContestListItem';
+import ContestListItem from './UI/ContestListItem/ContestListItem';
 
 const activeLink = classNames({'link': true, 'active': true});
 const dudUrl = 'javascript:;';
