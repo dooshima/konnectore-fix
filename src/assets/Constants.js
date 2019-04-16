8f59cb66e18ba54d9191b8d8350dee3ee25a2ae1
Saidi Adegoke <saidiadegokea@gmail.com>
2019-04-16 10:32:21 +0100

    add temp server

--- src/assets/Constants.js
+++ src/assets/Constants.js
@@ -5,7 +5,7 @@ const Constants = {
     "CLIENT_ID": 2,
     "CLIENT_SECRET": "qx7kdepPeBt2JuInD3OvtoQrHy6pz6pnGUXYcpWm",
     "GRANT_TYPE": "password",
-    "BASE_URL": "http://localhost/konnectoreapi/public/",
+    "BASE_URL": "https://82bd377e.ngrok.io/konnectoreapi/public/",
 }
 
 export default Constants;
