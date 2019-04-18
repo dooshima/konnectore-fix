25d38735646a43159047d515de89ff199078d960
Saidi Adegoke <saidiadegokea@gmail.com>
2019-04-18 11:54:03 +0100

    Handle null and undefined state on friend header summary component

--- src/components/UIC/Friend/FriendHeaderSummary.js
+++ src/components/UIC/Friend/FriendHeaderSummary.js
@@ -47,7 +47,8 @@ const FriendHeaderSummary = props => {
       onClick={() => this.followFriend}
       label="Follow" size="small" />
   })
-  const person = null !== currentUser && typeof(currentUser) !== 'undefined'? currentUser.user: {};
+  const person = null !== currentUser && typeof(currentUser) !== 'undefined'? (null !== currentUser.user && typeof(currentUser.user) !== 'undefined'? currentUser.user: {}): {};
+  console.log(person);
   const path = null !== person && typeof(person) !== 'undefined'? person.avatar: "";
   const avatar = Utility.getAvatar(path);
   const fname = null !== person && typeof(person) !== 'undefined'? person.firstname: '';
